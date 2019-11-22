e692e396a0f49dcf05cc79e8690d5c52a097b0e8
@alejo_mateus <ajimenez@avanzamas.com.co>
2019-11-21 21:29:40 -0500

    Use package Http for Nodejs and install express

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+http.createServer((req, res)=>{
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    let output = {
+        name: 'alejo',
+        years: 23,
+        url: req.url
+    }
+    res.write(output);
+    // res.write('Hello World');
+    res.end();
+})
+.listen(8080)
+console.log('Listen port 8080');
