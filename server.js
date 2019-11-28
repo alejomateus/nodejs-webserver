2eaaeb15c180b6a9f3c73f5b35641e805127b3a1
@alejo_mateus <ajimenez@avanzamas.com.co>
2019-11-27 19:00:53 -0500

    Use helpers to transform data using Handlebars

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 const express = require('express')
 const app = express();
 const hbs = require('hbs');
+require('./hbs/helpers')
 app.use(express.static(`${__dirname}/public`))
 hbs.registerPartials(`${__dirname}/views/partials`)
-app.set('view engine','hbs');
-app.get('/',  (req, res) =>{
-    res.render('home',{
-        name: 'Alejandro',
-        year: new Date().getFullYear()
+app.set('view engine', 'hbs');
+app.get('/', (req, res) => {
+    res.render('home', {
+        name: 'alejandro jimenez'
     });
 })
-app.get('/about',  (req, res) =>{
-    res.render('about',{
+app.get('/about', (req, res) => {
+    res.render('about', {
         name: 'Alejandro',
-        year: new Date().getFullYear()
     });
 })
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log('Listen 3000');
 })
